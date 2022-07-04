32a7446eb5a323397781a44608941e4f4432712f
lathiifAs <ajisanthoshol@gmail.com>
2022-07-04 15:55:22 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,205 +1,189 @@
-<!doctype html>
-<html class="no-js" lang="zxx">
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Wedding &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Weeding</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--
+	//////////////////////////////////////////////////////
 
-    <!-- <link rel="manifest" href="site.webmanifest"> -->
-    <!-- <link rel="shortcut icon" type="image/x-icon" href="{{ URL('public_template/img/favicon.png') }}"> -->
-    <!-- Place favicon.ico in the root directory -->
+	FREE HTML5 TEMPLATE
+	DESIGNED & DEVELOPED by FREEHTML5.CO
 
-    <!-- CSS here -->
-    <link rel="stylesheet" href="{{ URL('public_template/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/nice-select.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/gijgo.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/slicknav.css') }}">
-    <link rel="stylesheet" href="{{ URL('public_template/css/style.css') }}">
-    <!-- <link rel="stylesheet" href="{{ URL('public_template/css/responsive.css') }}"> -->
-</head>
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
 
-<body>
-    <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
+	//////////////////////////////////////////////////////
+	 -->
 
-    <!-- header-->
-    <header>
-        <div class="header-area ">
-            <div id="sticky-header" class="main-header-area">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-xl-3 col-lg-3">
-                            <div class="logo-img">
-                                <a href="index.html">
-                                    <img src="{{ URL('public_template/img/icon/FA_LOGO_DAT_COSTUME_PNG.png') }}" width="200px" alt="">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-9 col-lg-9">
-                            <div class="main-menu  d-none d-lg-block">
-                                <nav>
-                                    <ul id="navigation">
-                                        <li><a class="active" href="javascript:;">Beranda</a></li>
-                                        <li><a href="{{ route('login') }}">Login</a></li>
-                                        <li><a href="https://dayutekno.com">Tentang Kami</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/animate.css') }}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/icomoon.css') }}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/bootstrap.css') }}">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/magnific-popup.css') }}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/owl.theme.default.min.css') }}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{ URL('template_spesial/css/style.css') }}">
+
+	<!-- Modernizr JS -->
+	<script src="{{ URL('template_spesial/js/modernizr-2.6.2.min.js') }}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="{{ URL('template_spesial/js/respond.min.js') }}"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-5">
+					<div id="fh5co-logo"><a href="index.html">Wedding Inv<strong>.</strong></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="javascript:;">Beranda</a></li>
+						<li><a href="{{ route('login') }}">Login</a></li>
+						<li><a href="https://dayutekno.com">Tentang Kami</a></li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(template_spesial/images/img_bg_2.jpg); height: 650px;" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1 style="font-size: 55px;padding-top: 80px" >Nama Anda <br> &amp; <br> Pasangan Anda</h1>
+							<h2>Undangan Digital Mulai dari 100k <br> Buruan buat sebelum waktu dibawah habis !</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<p><a href="https://api.whatsapp.com/send/?phone=6281904597978&text=Hallo+Darma+Ayu+Tekno+,+Saya+Ingin+membuat+undangan+digital&app_absent=0" class="btn btn-default btn-sm">Buat Undangan</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+            <div class="row text-center">
+                <div class="row">
+                    <label for="">Informasi :</label>
                 </div>
+                    <a href="{{ URL('admin_template/files/undangan_paket.pdf') }}" class="btn btn-default col-md-6">Lihat Paket</a>
+                    <a href="{{ route('/demo') }}" class="btn btn-default col-md-6">Lihat Contoh Undangan</a>
             </div>
-        </div>
-    </header>
-    <!--/ header-->
+			<div class="row copyright" style="padding-top: 20px">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2016. All Rights Reserved.</small>
+						<small class="block">Created by <a href="https://dayutekno.com" target="_blank">Darma Ayu Tekno</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
 
-    <!-- slider_area -->
-    <div class="slider_area " id="undangan">
-        <div class="slider_area_inner slider_bg_1 overlay2">
-            <div class="slider_text text-center">
-                <div class="text_inner">
-                    <img src="{{ URL('public_template/img/banner/ornaments.png') }}" alt="">
-                    <h4>UNDANGAN ONLINE</h4>
-                    <h3 style="font-size: 20px">Anda & Pasangan Anda <br></h3>
+		</div>
+	</footer>
+	</div>
 
-                    <a href="https://api.whatsapp.com/send/?phone=6281904597978&text=Hallo+Darma+Ayu+Tekno+,+Saya+Ingin+membuat+undangan+digital&app_absent=0" class="btn btn-info"><span>Buat Sekarang !</span></a>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
 
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ slider_area -->
+	<!-- jQuery -->
+	<script src="{{ URL('template_spesial/js/jquery.min.js') }}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{ URL('template_spesial/js/jquery.easing.1.3.js') }}"></script>
+	<!-- Bootstrap -->
+	<script src="{{ URL('template_spesial/js/bootstrap.min.js') }}"></script>
+	<!-- Waypoints -->
+	<script src="{{ URL('template_spesial/js/jquery.waypoints.min.js') }}"></script>
+	<!-- Carousel -->
+	<script src="{{ URL('template_spesial/js/owl.carousel.min.js') }}"></script>
+	<!-- countTo -->
+	<script src="{{ URL('template_spesial/js/jquery.countTo.js') }}"></script>
 
-    <!-- wedding_countdown -->
-    <div class="weeding_countdown_area">
-        <div class="flowaers_top d-none d-lg-block">
-            <img src="{{ URL('public_template/img/banner/flower-top.png') }}" alt="">
-        </div>
-        <div class="flowaers_bottom d-none d-lg-block">
-                <img src="{{ URL('public_template/img/banner/flower-bottom.png') }}" alt="">
-            </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="section_title text-center">
-                        <img src="{{ URL('public_template/img/banner/flowers.png') }}" alt="">
-                        <span>Mulai dari Rp. 100K</span>
-                        <a  href="{{ URL('public_template/files/undangan_paket.pdf') }}" target="_blank" class="btn btn-outline-info text-success"><i class="fa fa-file"></i> <b class="text-success">Lihat Paket</b></a>
-                        <a  href="{{ route('/demo') }}" target="_blank" class="btn btn-outline-info text-info mt-3"><i class="fa fa-id-card"></i> <b class="text-info">Lihat Contoh Undangan</b></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ wedding_countdown -->
+	<!-- Stellar -->
+	<script src="{{ URL('template_spesial/js/jquery.stellar.min.js') }}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{ URL('template_spesial/js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ URL('template_spesial/js/magnific-popup-options.js') }}"></script>
 
-   <!-- footer_start -->
-   <footer class="footer mt-4">
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js') }}"></script> -->
+	<script src="{{ URL('template_spesial/js/simplyCountdown.js') }}"></script>
+	<!-- Main -->
+	<script src="{{ URL('template_spesial/js/main.js') }}"></script>
 
-    <div class="copy-right_text">
-        <div class="container">
-            <div class="footer_border"></div>
-            <div class="row">
-                <div class="col-xl-12">
-                    <p class="copy_right text-center">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | </i> by <a href="https://dayutekno.com" target="_blank">Darma Ayu Tekno</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- footer_end -->
+	<script>
+    var d = new Date(new Date().getTime() + 700 * 120 * 120 * 2000);
 
-    <!-- JS here -->
-    <script src="{{ URL('public_template/js/vendor/modernizr-3.5.0.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/vendor/jquery-1.12.4.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/popper.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/bootstrap.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/ajax-form.js') }}"></script>
-    <script src="{{ URL('public_template/js/waypoints.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.counterup.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/scrollIt.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.scrollUp.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/wow.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/nice-select.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/gijgo.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.countdown.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.slicknav.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/plugins.js') }}"></script>
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
 
-    <!--contact js-->
-    <script src="{{ URL('public_template/js/contact.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.ajaxchimp.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.form.js') }}"></script>
-    <script src="{{ URL('public_template/js/jquery.validate.min.js') }}"></script>
-    <script src="{{ URL('public_template/js/mail-script.js') }}"></script>
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+</script>
 
-    <script src="{{ URL('public_template/js/main.js') }}"></script>
-
-    <script>
-        $('#clock').countdown('2022/05/29', function (event) {
-            $(this).html(event.strftime(
-                '<div class="countdown_wrap d-flex"><div  class="single_countdown"><h3>%D</h3><span>Days</span></div><div class="single_countdown"><h3>%H</h3><span>Hours</span></div><div class="single_countdown"><h3>%M</h3><span>Minutes</span></div><div class="single_countdown"><h3>%S</h3><span>Seconds</span></div></div>'
-            ));
-        });
-    </script>
-    <script>
-        function initMap() {
-            var uluru = {
-                lat: -25.363,
-                lng: 131.044
-            };
-            var grayStyles = [{
-                    featureType: "all",
-                    stylers: [{
-                            saturation: -90
-                        },
-                        {
-                            lightness: 50
-                        }
-                    ]
-                },
-                {
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#ccdee9'
-                    }]
-                }
-            ];
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: -31.197,
-                    lng: 150.744
-                },
-                zoom: 9,
-                styles: grayStyles,
-                scrollwheel: false
-            });
-        }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpfS1oRGreGSBU5HHjMmQ3o5NLw7VdJ6I&amp;callback=initMap">
-    </script>
-
-</body>
-
+	</body>
 </html>
+
